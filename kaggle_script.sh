41cf6ae85b220fdf746891771f36600b32541ed3
Olga Kalinichenko <s15b1_kalinichenko@179.ru>
2018-11-17 19:21:31 +0300

    changed kernel

--- kaggle_script.sh
+++ kaggle_script.sh
@@ -1,4 +1,4 @@
 kaggle competitions download -c titanic -p ./input
-kaggle kernels pull -p ./kaggle_sol madshutt/titanic
-jupyter nbconvert --to notebook --execute ./kaggle_sol/titanic.ipynb --output ./kaggle_sol/titanic.ipynb
+kaggle kernels pull -p ./kaggle_sol madshutt/titanic2
+jupyter nbconvert --to notebook --execute ./kaggle_sol/titanic2.ipynb --output ./kaggle_sol/titanic2.ipynb
 kaggle competitions submit -c titanic -f ./kaggle_sol/submission.csv -m "Sample"
