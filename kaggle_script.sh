60b51695b5ce43b81e906fd4b9c6b7ef9e43912b
Olga Kalinichenko <s15b1_kalinichenko@179.ru>
2018-11-17 18:29:09 +0300

    added seaborn

--- kaggle_script.sh
+++ kaggle_script.sh
@@ -1,4 +1,4 @@
 kaggle competitions download -c titanic -p ./input
-kaggle kernels pull -p ./titanic.ipynb madshutt/Titanic\ Data
+kaggle kernels pull -p . madshutt/titanic
 jupyter nbconvert --to notebook --execute ./titanic.ipynb --output ./titanic.ipynb
 kaggle competitions submit -c titanic -f ./submission.csv -m "Sample solution"
