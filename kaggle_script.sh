395be8eaaef7dffe8e5092f7be83e85687c9a755
Olga Kalinichenko <s15b1_kalinichenko@179.ru>
2018-11-17 18:45:47 +0300

    testt

--- kaggle_script.sh
+++ kaggle_script.sh
@@ -1,4 +1,4 @@
 kaggle competitions download -c titanic -p ./input
 kaggle kernels pull -p . vprovv/sample-kernel
-jupyter nbconvert --to notebook --execute ./titanic.ipynb --output ./titanic.ipynb
+jupyter nbconvert --to notebook --execute ./sample-kernel.ipynb --output ./sample-kernel.ipynb
 kaggle competitions submit -c titanic -f ./submission.csv -m "Sample solution"
