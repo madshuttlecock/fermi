4cb72f18ef09129c8593af8501170ba94e96226c
Olga Kalinichenko <s15b1_kalinichenko@179.ru>
2018-11-17 19:33:43 +0300

    ............

--- kaggle_script.sh
+++ kaggle_script.sh
@@ -1,4 +1,4 @@
 kaggle competitions download -c titanic -p ./input
-kaggle kernels pull -p ./kaggle_sol madshutt/titanic3
-jupyter nbconvert --to notebook --execute ./kaggle_sol/titanic3.ipynb --output ./kaggle_sol/titanic3.ipynb
+kaggle kernels pull -p ./kaggle_sol madshutt/titanic
+jupyter nbconvert --to notebook --execute ./kaggle_sol/titanic.ipynb --output ./kaggle_sol/titanic.ipynb
 kaggle competitions submit -c titanic -f ./kaggle_sol/submission.csv -m "Sample"
