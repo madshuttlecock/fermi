9f4840a1b1f5f98f9ef70ad6e667e857af422273
Olga Kalinichenko <s15b1_kalinichenko@179.ru>
2018-11-17 18:13:13 +0300

    changed kaggle-script

--- kaggle_script.sh
+++ kaggle_script.sh
@@ -1,4 +1,4 @@
 kaggle competitions download -c titanic -p ./input
 kaggle kernels pull -p ./tasks madshutt/titanic
 jupyter nbconvert --to notebook --execute ./tasks/titanic.ipynb --output ./tasks/titanic.ipynb
-kaggle competitions submit -c titanic -f ./tasks/solution.csv -m "Sample solution"
+kaggle competitions submit -c titanic -f ./tasks/submission.csv -m "Sample solution"
